b1e155be6585a50413f8d23cc79bd5f114fbf1b2
codecommituser-at-615289500642 <vikas@kyndryl.com>
2024-03-09 07:29:39 -0500

    new update

--- file.java
+++ file.java
@@ -1,4 +1,8 @@
 hello
 this is by feature branch.
 this is update in remote main branch
-want to show ftech command usage
+want to show ftech command usagei
+bug2
+=======
+man usage of something.
+
